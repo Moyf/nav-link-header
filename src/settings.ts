8290f7ca100692b23b42eb17e72774a11c60aeb7
ahts4962 <181621037+ahts4962@users.noreply.github.com>
2024-12-24 15:16:54 +0900

    Apply code formatting and fix code to pass linting

--- src/settings.ts
+++ src/settings.ts
@@ -14,7 +14,7 @@ export interface NavLinkHeaderSettings {
 	yearlyNoteLinksEnabled: boolean;
 	displayPlaceholder: boolean;
 	confirmFileCreation: boolean;
-	propertyMappings: Array<{property: string, emoji: string}>;
+	propertyMappings: Array<{ property: string; emoji: string }>;
 	filterDuplicateNotes: boolean;
 	usePropertyAsDisplayName: boolean;
 	displayPropertyName: string;
@@ -34,7 +34,7 @@ export const DEFAULT_SETTINGS: NavLinkHeaderSettings = {
 	yearlyNoteLinksEnabled: false,
 	displayPlaceholder: false,
 	confirmFileCreation: true,
-	propertyMappings: [{property: "up", emoji: "⬆️"}],
+	propertyMappings: [{ property: "up", emoji: "⬆️" }],
 	filterDuplicateNotes: true,
 	usePropertyAsDisplayName: false,
 	displayPropertyName: "title",
@@ -253,8 +253,10 @@ export class NavLinkHeaderSettingTab extends PluginSettingTab {
 					"Each line should be in the format 'property:emoji'."
 			)
 			.addTextArea((text) => {
-				const mappings = this.plugin.settings!.propertyMappings
-					.map((mapping) => `${mapping.property}:${mapping.emoji}`)
+				const mappings = this.plugin
+					.settings!.propertyMappings.map(
+						(mapping) => `${mapping.property}:${mapping.emoji}`
+					)
 					.join("\n");
 				
 				const settingItem = text.inputEl.parentElement?.parentElement;
@@ -275,12 +277,16 @@ export class NavLinkHeaderSettingTab extends PluginSettingTab {
 				text.setValue(mappings)
 					.setPlaceholder("up:⬆️\nparent:👆\nsource:📚")
 					.onChange(async (value) => {
-						const newMappings = value.split("\n")
-							.map(line => line.trim())
-							.filter(line => line.length > 0)
+						const newMappings = value
+							.split("\n")
+							.map((line) => line.trim())
+							.filter((line) => line.length > 0)
 							.map((mapping) => {
 								const [property, emoji] = mapping.split(":");
-								return { property: property.trim(), emoji: emoji?.trim() || "⬆️" };
+								return {
+									property: property.trim(),
+									emoji: emoji?.trim() || "⬆️",
+								};
 							});
 						this.plugin.settings!.propertyMappings = newMappings;
 						this.plugin.app.workspace.trigger(
@@ -332,17 +338,15 @@ export class NavLinkHeaderSettingTab extends PluginSettingTab {
 						"If left blank, the property name will not be displayed."
 				)
 				.addText((text) => {
-					text
-						.setValue(this.plugin.settings!.displayPropertyName)
-						.onChange(
-							async (value) => {
-								this.plugin.settings!.displayPropertyName = value;
-								this.plugin.app.workspace.trigger(
-									"nav-link-header:settings-changed"
-								);
-								await this.plugin.saveSettings();
-							}
+					text.setValue(
+						this.plugin.settings!.displayPropertyName
+					).onChange(async (value) => {
+						this.plugin.settings!.displayPropertyName = value;
+						this.plugin.app.workspace.trigger(
+							"nav-link-header:settings-changed"
 						);
+						await this.plugin.saveSettings();
+					});
 				});
 		}
 
